34ab8b83dbeff266425bacb4f263303e03b17d09
Hugo <hugolindoso.c@gmail.com>
2024-06-13 21:25:33 -0300

    feat: dependency injection

--- BubberDinner.Api/Program.cs
+++ BubberDinner.Api/Program.cs
@@ -1,12 +1,13 @@
-using BubberDinner.Application.Services.Authentication;
+using BubberDinner.Application;
+using BubberDinner.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
+builder.Services
+    .AddApplication()
+    .AddInfrastructure();
 builder.Services.AddControllers();
 
 var app = builder.Build();
-
 app.UseHttpsRedirection();
 app.MapControllers();
 app.Run();
