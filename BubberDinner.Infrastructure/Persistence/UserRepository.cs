401a331a5067b0219e8a53b8b4b01032c519f94d
Hugo <hugolindoso.c@gmail.com>
2024-06-18 21:27:10 -0300

    feat: comentario sobre addscoped

--- BubberDinner.Infrastructure/Persistence/UserRepository.cs
+++ BubberDinner.Infrastructure/Persistence/UserRepository.cs
@@ -5,6 +5,9 @@ namespace BubberDinner.Infrastructure.Persistence;
 
 public class UserRepository : IUserRepository
 {
+    //Por ser static anula o comportamento do AddScoped
+    //com relação a recriar a lista a cada requisição.
+    //Usando static a variavel pertence a classe e nao ao objeto
     private static readonly List<User> _users = [];
     public void AddUser(User user)
     {
